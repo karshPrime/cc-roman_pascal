639f5e6896f2a22fb94537d2d557f48f9b5dc3d5
ut-kr <utkarsh@cyberfear.com>
2022-02-21 05:00:29 +1100

    � base code

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>;
+#include <vector>;
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::string;
+
+int main() {
+  cout << "Generate Pascal trianle in Roman Numbers!" << endl;
+  cout << endl << "Enter number of rows: ";
+  short layers;
+  cin >> layers;
+
+  return 0;
+}
