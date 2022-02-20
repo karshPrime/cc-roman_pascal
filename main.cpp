4344946b46edcf7ac9921a831c866f3d481297c9
ut-kr <utkarsh@cyberfear.com>
2022-02-21 05:02:34 +1100

    � functions

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ using std::endl;
 using std::vector;
 using std::string;
 
+void print_table(vector<vector<short>>& triangle);
+void pascal_generate(short total_layers, vector<vector<short>>& triangle);
+
 int main() {
   cout << "Generate Pascal trianle in Roman Numbers!" << endl;
   cout << endl << "Enter number of rows: ";
@@ -14,4 +17,21 @@ int main() {
   cin >> layers;
 
   return 0;
+}
+
+// generate whole pascal triangle
+void pascal_generate(short total_layers, vector<vector<short>>& triangle) {
+  // code
+}
+
+
+// convert one integer into roman number
+string convert_romans(short single_integer) {
+    // code
+}
+
+
+// prints the whole triangle, row by row.
+void print_table(vector<vector<short>>& triangle) {
+  // code
 }
